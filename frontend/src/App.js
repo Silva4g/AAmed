0af56592ccea4d309934711d3e1f38247bda4236
Gabriee <gabrie12e3@hotmail.com>
2020-02-14 10:55:04 -0300

    inicio do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form encType={}>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
