a9197905869709747473572d73365cc4738355c1
Felipe Carradori <56209331+Carradori@users.noreply.github.com>
2020-02-22 15:54:45 -0300

    arrumei o menu

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import $ from 'jquery';
-import {findDOMNode} from 'react-dom';
-
 import "./App.css";
 import Menu from "./components/Menu/index";
 import Rodape from "./components/Rodape/index";
@@ -11,23 +8,11 @@ import defaults from './Pages/index';
 const { Home, Picture, Support, About, Login, Register } = defaults;
 
 export default class App extends Component {
-  componentDidMount() {
-    var lastScroll = 0;
-    const el = findDOMNode(this.refs.menu);
-    $(window).scroll(function (e) {
-      var st = $(this).scrollTop();
-      if (st > lastScroll) {
-        $(el).addClass('hide');
-      } else {
-        $(el).removeClass('hide');
-      }
-      lastScroll = st;
-    });
-  }
+  
   render() {
     return (
       <Router>
-        <Menu ref="menu"/>
+        <Menu/>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/picture" component={Picture} />
--- web/src/components/Menu/index.js
+++ web/src/components/Menu/index.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 
+import $ from 'jquery';
+import {findDOMNode} from 'react-dom';
+
 import "./style.css";
 import logo from "../../assets/logo.png";
 
 export default class Menu extends Component {
+  componentDidMount() {
+    var lastScroll = 0;
+    const el = findDOMNode(this.refs.menu);
+    $(window).scroll(function (e) {
+      var st = $(this).scrollTop();
+      if (st > lastScroll) {
+        $(el).css('transform', 'translateY(-120px)');
+      } else {
+        $(el).css('transform', 'none');
+      }
+      lastScroll = st;
+    });
+  }
   render() {
     return (
 
       <div className="content">
-        <div className="menu">
+        <div className="menu" ref="menu">
 
           <div className="logo">
             <img src={logo} alt="logo" />
