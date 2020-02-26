5aaca736990801dd983738ab44745f55658994a8
Felipe Carradori <56209331+Carradori@users.noreply.github.com>
2020-02-26 18:28:40 -0300

    teste menu diferente

--- web/src/components/Menu/index.js
+++ web/src/components/Menu/index.js
@@ -9,6 +9,14 @@ import logo from "../../assets/logo.png";
 
 export default class Menu extends Component {
   componentDidMount() {
+    var node = findDOMNode(this.refs.btn);
+    var links = findDOMNode(this.refs.links);
+
+    node.addEventListener('click', () => {
+      links.classList.toggle('nav-active');
+      node.classList.toggle('toggle');
+    });
+
     var lastScroll = 0;
     const el = findDOMNode(this.refs.menu);
     $(window).scroll(function (e) {
@@ -24,52 +32,81 @@ export default class Menu extends Component {
   render() {
     return (
 
+      // <div className="content">
+      //   <div className="menu" ref="menu">
+
+      //     <div className="logo">
+      //       <Link to="/">
+      //         <img src={logo} alt="logo" />
+      //       </Link>
+      //     </div>
+
+      //     <div className="itemsMenu">
+      //       <ul>
+      //         <li>
+      //           <Link to="/">
+      //             HOME
+      //           </Link>
+      //         </li>
+      //         <li>
+      //           <Link to="/picture">
+      //             FOTOS
+      //           </Link>
+      //         </li>
+      //         <li>
+      //           <Link to="/support">
+      //             SUPORTE
+      //           </Link>
+      //         </li>
+      //         <li>
+      //           <Link to="/about">
+      //             SOBRE
+      //           </Link>
+      //         </li>
+      //       </ul>
+      //     </div>
+
+      //     <div className="signInUp">
+      //       <Link to="/login">LOGIN</Link>
+      //       <Link to="/register">
+      //         <button>CADASTRAR</button>
+      //       </Link>
+      //     </div>
       <div className="content">
         <div className="menu" ref="menu">
-
           <div className="logo">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-          </div>
-
-          <div className="itemsMenu">
-            <ul>
-
-              <li>
-                <Link to="/">
-                  HOME
-                </Link>
-              </li>
-
-
-              <li>
-                <Link to="/picture">
-                  FOTOS
-                </Link>
-              </li>
-
-
-              <li>
-                <Link to="/support">
-                  SUPORTE
-                </Link>
-              </li>
-              <li>
-                <Link to="/about">
-                  SOBRE
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="signInUp">
-            <Link to="/login">LOGIN</Link>
-            <Link to="/register">
-              <button>CADASTRAR</button>
+            <Link to="/">
+              <img src={logo} alt="logo" />
             </Link>
           </div>
+          <div className="itemsMenu" id="links" ref="links">
+            <ul>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <Link to="/picture">FOTOS</Link>
+              </li>
+              <li>
+                <Link to="/support">SUPORTE</Link>
+              </li>
+              <li>
+                <Link to="/about">SOBRE</Link>
+              </li>
+            </ul>
 
+            <div className="signInUp">
+              <Link to="/login">LOGIN</Link>
+              <Link to="/register">
+                <button>CADASTRAR</button>
+              </Link>
+            </div>
+          </div>
+          <div className="burger" id="btn" ref="btn">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
         </div>
       </div>
     );
