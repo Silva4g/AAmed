f55a34ce0741a924480599a7d80c4a6d39d881b5
v-rapha <vs.raphael19@gmail.com>
2020-02-28 10:50:07 -0300

    not found ui

--- web/src/Pages/NotFound/index.js
+++ web/src/Pages/NotFound/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-
-// import { Container } from './styles';
+import logo from "../../assets/logo.png";
+import { Link } from 'react-router-dom';
+import { FaAngleLeft } from 'react-icons/fa';
+import './styles.css';
 
 export default class NotFound extends Component {
   render() {
-    return <h1>Página não encontrada</h1>;
+    return (
+      <div className="wrapper-404">
+        <header>
+          <nav>
+            <div className="back">
+              <Link to="/"><span><FaAngleLeft />Back</span></Link>
+            </div>
+          </nav>
+
+          <div className="broken">
+            <p className="upper">Oops! 404 - Not found.</p>
+            <img src={logo} alt="logo broken" />
+            <p className="lower">Não conseguimos encontrar a página que você está solicitando.</p>
+            <Link to="/" className="btnHome">Voltar a página inicial</Link>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
