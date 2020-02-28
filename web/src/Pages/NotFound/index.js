4e128608dff33d428b2ec9a5d07e71b7791782d0
Carradori <56209331+Carradori@users.noreply.github.com>
2020-02-28 08:19:59 -0300

    not found

---
+++ web/src/Pages/NotFound/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class NotFound extends Component {
+  render() {
+    return <h1>Página não encontrada</h1>;
+  }
+}
