085d18a1df5c8a57635b6db9f5011cfa1bc1ef16
ViniPiantoni <57470427+ViniPiantoni@users.noreply.github.com>
2020-03-09 10:52:40 -0300

    Tela Login e BurnDown

--- mobile/src/screens/Login/styles.js
+++ mobile/src/screens/Login/styles.js
@@ -1,111 +1,86 @@
 import { StyleSheet } from 'react-native';
-import {Dimensions} from 'react-native';
+import { Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
 
-    loginPrincipal: {
-      height: 380,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#52c8fa'
-    },
-  
-    /* image:{
-      flex: 1,
-      width: Dimensions.get('window').width,
-      height:450,
-      position:'relative'
-    }*/
-  
-    formulario:{
-      width:300,
-      height: 200,
-      //backgroundColor: 'yellow',
-      bottom:180,
-      position:'absolute'
-    },
-  
-    textoLogin:{
-      //fontFamily:'helvetica',
-      color:'#dbfeff',
-      fontSize:60,
-      textAlign:'center',
-      marginTop:25,
-    },
-  
-    inputEmail:{
-      flex:1,
-      width:300,
-      height:50,
-      marginTop:20,
-      textAlign:'center',
-      fontSize: 15,
-    },
-  
-    inputPassword:{
-      flex:1,
-      width:300,
-      height:50,
-      marginTop:30,
-      textAlign:'center',
-      fontSize: 15,
-    },
+  /* image:{
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height:450,
+    position:'relative'
+  }*/
 
-    botaoEntrar:{
-      backgroundColor:'#ffffff',
-      marginTop:20,
-      width:140,
-      height:60,
-      alignItems:'center',
-      alignSelf: 'center', 
-      justifyContent: 'center',
-      borderRadius:7,
-    },
+  textoLogin: {
+    //fontFamily:'helvetica',
+    color: '#dbfeff',
+    fontSize: 40,
+    alignSelf: 'center',
+    marginTop: 15
+  },
 
-    txtBtEntrar:{
-      fontSize: 15,
-      textAlign:'center',
-    },
+  div: {
+    paddingHorizontal: 30
+  },
 
-    containerEmail:{
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-    },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    containerPassword:{
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-    },
+  input: {
+    flex: 1,
+    height: 44,
+    marginTop: 20,
+    fontSize: 16,
+    paddingHorizontal: 40,
+  },
 
-    iconUser:{
-      color:'black',
-      fontSize:30,
-      position: 'absolute',
-      paddingTop:20,
-      paddingLeft: 15,
-    },
+  botaoEntrar: {
+    backgroundColor: '#ffffff',
+    marginTop: 20,
+    width: 140,
+    height: 60,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
 
-    iconLock:{
-      color:'black',
-      fontSize:30,
-      position: 'absolute',
-      paddingTop:30,
-      paddingLeft: 15,
-    },
+  txtBtEntrar: {
+    fontSize: 15,
+    textAlign: 'center',
+  },
 
-    iconEye:{
-      color:'black',
-      fontSize:30,
-      position: 'absolute',
-      paddingTop:30,
-      paddingLeft: 250,
-    },
+
+
+  iconUser: {
+    color: 'black',
+    fontSize: 30,
+    position: 'absolute',
+    top: 15,
+    left: 7,
+    zIndex: 5,
+  },
+
+  iconLock: {
+    color: 'black',
+    fontSize: 30,
+    position: 'absolute',
+    top: 15,
+    left: 7,
+    zIndex: 5
+  },
+
+  iconEye: {
+    color: 'black',
+    fontSize: 30,
+    position: 'absolute',
+    top: 15,
+    right: 10,
+    zIndex: 5
+  },
 
 });
 
 export default styles;
-    
