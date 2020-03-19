e2caffabbdd32b204e86cb7808e62f40867f6f05
Kauan <kauanssousa@gmail.com>
2020-03-19 14:06:45 -0300

    tentando colocar o modal pt2

--- web/src/Pages/Hospitals/index.js
+++ web/src/Pages/Hospitals/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, Component } from 'react';
-import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+import { Modal, Button, Row, Col, Form } from 'react-bootstrap';
+import { Button, ButtonToolBar } from 'react-bootstrap';
+import { AddDepModal } from '../../components/Modal/AddDepModal'
 
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { MdLocationOn } from 'react-icons/md';
@@ -7,36 +9,41 @@ import { MdLocationOn } from 'react-icons/md';
 import api from '../../services/api';
 import './styles.css';
 
-export default function (props) {
+class Hospital extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { deep:[], addModalShow : false }
+    // }
 
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [cnpj, setCnpj] = useState('');
-    const [cnes, setCnes] = useState('');
-    const [city, setCity] = useState('');
-    const [state, setState] = useState('');
-    const [street, setStreet] = useState('');
-    const [neighborhood, setNeighborhood] = useState('');
-    const [cep, setCep] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [phone, setPhone] = useState('');
+    // const [cnpj, setCnpj] = useState('');
+    // const [cnes, setCnes] = useState('');
+    // const [city, setCity] = useState('');
+    // const [state, setState] = useState('');
+    // const [street, setStreet] = useState('');
+    // const [neighborhood, setNeighborhood] = useState('');
+    // const [cep, setCep] = useState('');
 
-    const [hosp, setHosp] = useState([]);
+    // const [hosp, setHosp] = useState([]);
 
-    const getHospital = async () => {
-            const response = await api.get('/hospital', /*{
-            //     headers: { Authorization: `Bearer ${localStorage.getItem('tk-hopt')}` }
-            // }*/);
-            console.log(response.data);
-            setHosp(response.data);
-    }
-    useEffect(() => {
-        getHospital();
-    }, []);
+    // const getHospital = async () => {
+    //         const response = await api.get('/hospital', /*{
+    //         //     headers: { Authorization: `Bearer ${localStorage.getItem('tk-hopt')}` }
+    //         // }*/);
+    //         console.log(response.data);
+    //         setHosp(response.data);
+    // }
+    // useEffect(() => {
+    //     getHospital();
+    // }, []);
+
+    render () {
+
+        let addModalClose =() => this.setState({addModalShow:false});
 
     return (
         <>
@@ -51,13 +58,11 @@ export default function (props) {
             <div className="lista">
                 {hosp.map(hospital => (
                     <>
-                        <div className="inter">
-                            <img src={require('../../assets/logo.png')} alt="" />
-                            <h1>{hospital.name}</h1>
-                        </div>
+                        
                     </>
                     ))}
             </div>
         </>
     );
+}
 }
