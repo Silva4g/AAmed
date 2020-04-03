7a538281e865198bd420bdc39ec3413bf3bc99ae
Felipe Carradori <56209331+Carradori@users.noreply.github.com>
2020-04-03 16:53:25 -0300

    comunicação de chamadas do user com o hospital via sockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
-const path = require('path');
-const socketio = require('socket.io');
-const http = require('http');
-
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
 const server = http.Server(app);
 //conexão com o banco de dados mongo local
 mongoose.connect("mongodb://localhost:27017/tcc", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 //conexão do websocket
@@ -22,28 +21,34 @@ const io = socketio(server);
 
 //o certo seria colocar no banco, dps eu vejo isso
 const connectedUsers = {}; //id_user: id_socket
-let count = 0;
-io.on('connection', socket => {
-    count++;
-    const { hospital } = socket.handshake.query //pegar o id do hospital logado
-    console.log('hospital conectado ' + count + ' id: ' + socket.id + ' hospital: ' + hospital);
-    connectedUsers[hospital] = socket.id;
+io.on("connection", (socket) => {
+  console.log("socket conectado " + socket.id);
+  const { hospital_id } = socket.handshake.query;
+  connectedUsers[hospital_id] = socket.id;
+
+  //vai funcionar no mobile quando o user clicar em uma opção, o código de exemplo está na
+  //pasta 'mobile/example'
+  socket.on("hospitals_id", (data) => {
+    data.ids.allIds.map((ids) => {
+      const ownerSocket = connectedUsers[ids];
+      if (ownerSocket) {
+        socket //aqui os hospitais perto vão receber esse chamado
+          .to(ownerSocket)
+          .emit("aviso", { user: data.email, user_id: data.user_id });
+      }
+    });
+  });
 });
-
-app.use((req, res, next) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
-
-    return next();
-});
-
 
 app.use(cors());
 //uso do json para envio e recebimento de dados
 app.use(express.json());
 //url de fotos
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 app.use(routes);
 //log de requisições http
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 server.listen(3333);
--- web/src/Pages/Logged/index.js
+++ web/src/Pages/Logged/index.js
@@ -1,51 +1,63 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 //socket io
-import socketio from 'socket.io-client';
-import { IoMdSettings } from 'react-icons/io';
-import { AiFillMedicineBox } from 'react-icons/ai';
-import { FaUserCircle, FaList } from 'react-icons/fa';
+import socketio from "socket.io-client";
+import { IoMdSettings } from "react-icons/io";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { FaUserCircle, FaList } from "react-icons/fa";
 
-import './styles.css';
-import { useEffect } from 'react';
-import api from '../../services/api';
+import "./styles.css";
+import { useEffect } from "react";
+import api from "../../services/api";
 
 export default function Logged() {
-
   let click = React.createRef();
   let changeColor = React.createRef();
 
   const [id, setId] = useState(null);
+  //requisição do socket
+  const [user, setUser] = useState([]);
+  const [ok, setOk] = useState(null);
 
   useEffect(() => {
     async function getIdLogged() {
-      const response = await api.get('/hospital/home', {
-        headers: { Authorization: `Bearer ${localStorage.getItem('tk-hopt')}` }
+      const response = await api.get("/hospital/home", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("tk-hopt")}` },
       });
       setId(response.data._id);
     }
     getIdLogged();
 
-    const socket = socketio('http://localhost:3333', {
-      query: { hospital: id }
+    const socket = socketio("http://localhost:3333", {
+      query: { hospital: id },
     });
-
-  }, [id]);
-
-
+    socket.on("aviso", (data) => {
+      setUser([...user, data]);
+      setOk(true);
+    });
+  }, [user, id]);
 
   useEffect(() => {
     const evt = click.current;
     const cc = changeColor.current;
-    evt.addEventListener('click', () => {
-      cc.classList.toggle('bgcolorClick');
+    evt.addEventListener("click", () => {
+      cc.classList.toggle("bgcolorClick");
     });
   }, [changeColor, click]);
 
   return (
     <div className="container-logged">
+      {ok
+        ? user.map((users) => (
+            <div key={users.user_id}>
+              <div className="user-help">
+                {user.user} está solicitando uma ajuda!
+              </div>
+            </div>
+          ))
+        : ""}
       <div className="grid">
-        <img src={require('../../assets/hospital.jpg')} alt="" />
+        <img src={require("../../assets/hospital.jpg")} alt="" />
         <Link to="/profile" className="items">
           <FaUserCircle size={25} />
           <span>Perfil</span>
@@ -54,7 +66,9 @@ export default function Logged() {
           <div ref={changeColor}>
             <IoMdSettings size={25} />
             <input type="checkbox" className="hidden" id="toggle" />
-            <label htmlFor="toggle" className="click" ref={click}>Configurações</label>
+            <label htmlFor="toggle" className="click" ref={click}>
+              Configurações
+            </label>
             <ul className="info-config">
               <Link to="/update">Atualizar</Link>
               <Link to="/changepassword">Trocar senha</Link>
@@ -70,7 +84,7 @@ export default function Logged() {
           <AiFillMedicineBox size={25} />
           <span>Atendimentos</span>
         </Link>
-        <Link to='/logout' className="items logout">
+        <Link to="/logout" className="items logout">
           <span>Sair</span>
         </Link>
       </div>
