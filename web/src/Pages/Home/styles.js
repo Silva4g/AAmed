a58e8c50c0ed85ab22a5302ad84e677dd03f0d82
Kauan <kauanssousa@gmail.com>
2020-07-08 00:09:04 -0300

    fornt-end

--- web/src/Pages/Home/styles.js
+++ web/src/Pages/Home/styles.js
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div `
     height: 100%;
     width: 100%;
     display: flex;
     align-content: space-between;
 
     img {
-        width: 1000px;
+        width: 800px;
     }
 `;
-export const Main = styled.div`
+export const Main = styled.div `
     display: flex;
     flex-direction: column;
     justify-content: center;
+    margin: 0 100px;
 
     h2{
         color: #ccc;
+        margin-left: 15%;
     }
     img{
         width: 600px;
